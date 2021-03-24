38e72b2ae2480cfbdd203e0f6748f32503426d0f
Santi Arnau <santi07a@gmail.com>
2021-03-24 22:09:24 +0100

    deploy en netlify

--- src/Frases.js
+++ src/Frases.js
@@ -1,19 +1,24 @@
 const frases = [
     "Endereza tu espalda",
     "Toma 4 vasos de agua al día",
-    "Los cítricos no son buenos con el estómago vacio",
     "Respira profundo por un minuto",
     "No te estreses por cosas insignificantes",
     "Recuerda decirle lo mucho que la quieres a esa persona",
     "Medita 20 minutos al día",
     "Lleva tus bolsas al super",
+    "Mide tus palabras",
+    "Piensa antes de hablar",
     "Manten limpio tu cuarto",
     "Deja de postergar las cosas",
     "Haz más deporte",
     "Riega tus plantas",
     "No comas azúcar antes de dormir",
     "No compres cosas innecesarias",
-    "Hay algo que sabes hacer muy bien, compártelo con otros. Enséñalo a alguien más"
+    "Hay algo que sabes hacer muy bien, compártelo con otros. Enséñalo a alguien más",
+    "Sé impecable con tus palabras",
+    "No te tomes nada personal",
+    "Tiende tu cama",
+    "Rie más",
 ]
 
 export default frases;
